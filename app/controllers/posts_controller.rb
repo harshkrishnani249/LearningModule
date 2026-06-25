35bf2350c8a879775c4c0aac73fc660ef48b3246
Harsh Krishnani <harshkrishnani2409@gmail.com>
2026-06-25 13:08:12 +0530

    feat: configure Redis client and add cached JSON response to PostsController#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user, :comments).order(created_at: :desc).limit(20)
+    @posts = Post.includes(:user, :comments).all
+
+    respond_to do |format|
+      format.html
+      format.json do
+        data = Rails.cache.fetch("api/posts/index", expires_in: 5.minutes) do
+          Post.where(status: "published").includes(:user, :category).map do |post|
+            { id: post.id, title: post.title, author: post.user.name, category: post.category.name }
+          end
+        end
+        render json: data
+      end
+    end
   end
 
   def show
